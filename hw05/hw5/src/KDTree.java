9a7503525ed687e7e7d31cac0143c525580b3aeb
Joseph home <joseph122792@gmail.com>
2025-04-10 17:59:41 +0800

    finish hw05

--- hw05/hw5/src/KDTree.java
+++ hw05/hw5/src/KDTree.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Vector;
 
 public class KDTree {
@@ -12,55 +15,203 @@ public class KDTree {
 	}
 
 	boolean compare(double[] a) {
-		throw(new Error("TODO"));
+		int ra = depth % a.length;
+		int rp = depth % this.point.length;
+		return a[ra] >= this.point[rp];
 	}
 
 	static KDTree insert(KDTree tree, double[] p) {
-		throw(new Error("TODO"));
+		if (tree == null) {
+			return new KDTree(p, 0);
+		}
+		boolean isLeft = !tree.compare(p);
+		if (isLeft) {
+			if (tree.left == null) {
+				tree.left = new KDTree(p, tree.depth + 1);
+			} else {
+				insert(tree.left, p);
+			}
+		} else {
+			if (tree.right == null) {
+				tree.right = new KDTree(p, tree.depth + 1);
+			} else {
+				insert(tree.right, p);
+			}
+		}
+		return tree;
 	}
 
 	static double sqDist(double[] a, double[] b) {
-		throw(new Error("TODO"));
+		double res = 0;
+		for (int i = 0; i < a.length; i++) {
+			double d = a[i] - b[i];
+			res += d * d;
+		}
+		return res;
 	}
 
 	static double[] closestNaive(KDTree tree, double[] a, double[] champion) {
-		throw(new Error("TODO"));
+		if (tree == null) {
+			return champion;
+		}
+
+		if (champion == null || sqDist(tree.point, a) < sqDist(champion, a)) {
+			champion = tree.point;
+		}
+
+		champion = closestNaive(tree.left, a, champion);
+		champion = closestNaive(tree.right, a, champion);
+
+		return champion;
 	}
 
-
 	static double[] closestNaive(KDTree tree, double[] a) {
-		throw(new Error("TODO"));
+		return closestNaive(tree, a, null);
 	}
 
 	static double[] closest(KDTree tree, double[] a, double[] champion) {
 		if (tree == null)
 			return champion;
 
-		// sert pour InteractiveClosest.
 		InteractiveClosest.trace(tree.point, champion);
-		
-		throw(new Error("TODO"));
+
+		if (champion == null || sqDist(tree.point, a) < sqDist(champion, a)) {
+			champion = tree.point;
+		}
+
+		int axis = tree.depth % a.length;
+
+		boolean goLeft = a[axis] < tree.point[axis];
+		KDTree first = goLeft ? tree.left : tree.right;
+		KDTree second = goLeft ? tree.right : tree.left;
+
+		champion = closest(first, a, champion);
+
+		if (Math.abs(a[axis] - tree.point[axis]) < Math.sqrt(sqDist(champion, a))) {
+			champion = closest(second, a, champion);
+		}
+
+		return champion;
 	}
 
 	static double[] closest(KDTree tree, double[] a) {
-		throw(new Error("TODO"));
+		return closest(tree, a, null);
+	}
+
+	static double[] farthest(KDTree tree, double[] a, double[] champion) {
+		if (tree == null)
+			return champion;
+
+		InteractiveClosest.trace(tree.point, champion);
+
+		if (champion == null || sqDist(tree.point, a) > sqDist(champion, a)) {
+			champion = tree.point;
+		}
+
+		int axis = tree.depth % a.length;
+
+		boolean goLeft = a[axis] < tree.point[axis];
+		KDTree first = goLeft ? tree.left : tree.right;
+		KDTree second = goLeft ? tree.right : tree.left;
+
+		champion = farthest(first, a, champion);
+
+		if (Math.abs(a[axis] - tree.point[axis]) > Math.sqrt(sqDist(champion, a))) {
+			champion = farthest(second, a, champion);
+		}
+
+		return champion;
+	}
+
+	static double[] farthest(KDTree tree, double[] a) {
+		return farthest(tree, a, null);
 	}
 
 	static int size(KDTree tree) {
-		throw(new Error("TODO"));
+		if (tree == null) {
+			return 0;
+		}
+		return 1 + size(tree.left) + size(tree.right);
 	}
 
 	static void sum(KDTree tree, double[] acc) {
-		throw(new Error("TODO"));
+		if (tree == null) {
+			return;
+		}
+		for (int i = 0; i < acc.length; i++) {
+			acc[i] += tree.point[i];
+		}
+		sum(tree.left, acc);
+		sum(tree.right, acc);
 	}
 
 	static double[] average(KDTree tree) {
-		throw(new Error("TODO"));
+		if (tree == null) {
+			return null;
+		}
+		double[] acc = new double[tree.point.length];
+		sum(tree, acc);
+		for (int i = 0; i < acc.length; i++) {
+			acc[i] /= size(tree);
+		}
+		return acc;
 	}
 
+	static void traversalTree(KDTree tree, Vector<double[]> points) {
+		if (tree == null)
+			return;
+		points.add(tree.point);
+		traversalTree(tree.left, points);
+		traversalTree(tree.right, points);
+	}
 
 	static Vector<double[]> palette(KDTree tree, int maxpoints) {
-		throw(new Error("TODO"));
+		// Farthest Point Sampling
+		Vector<double[]> palette = new Vector<>();
+		if (tree == null || maxpoints <= 0)
+			return palette;
+
+		Vector<double[]> allPoints = new Vector<>();
+		traversalTree(tree, allPoints);
+
+		if (allPoints.isEmpty())
+			return palette;
+
+		int total = allPoints.size();
+		double[] minDists = new double[total];
+
+		Random rand = new Random();
+		int firstIndex = rand.nextInt(total);
+		double[] first = allPoints.get(firstIndex);
+		palette.add(first);
+
+		for (int i = 0; i < total; i++) {
+			minDists[i] = sqDist(allPoints.get(i), first);
+		}
+
+		while (palette.size() < maxpoints) {
+			double maxDist = -1;
+			int farthestIndex = -1;
+
+			for (int i = 0; i < total; i++) {
+				if (minDists[i] > maxDist) {
+					maxDist = minDists[i];
+					farthestIndex = i;
+				}
+			}
+
+			double[] next = allPoints.get(farthestIndex);
+			palette.add(next);
+
+			for (int i = 0; i < total; i++) {
+				double d = sqDist(allPoints.get(i), next);
+				if (d < minDists[i]) {
+					minDists[i] = d;
+				}
+			}
+		}
+
+		return palette;
 	}
 
 	public String pointToString() {
--- hw05/hw5/src/TestSqDist.java
+++ hw05/hw5/src/TestSqDist.java
@@ -1,8 +1,39 @@
-
 public class TestSqDist {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
+        if (!TestSqDist.class.desiredAssertionStatus()) {
+            System.err.println("You must pass the -ea option to the Java Virtual Machine.");
+            System.exit(1);
+        }
 
-	}
+        System.out.println("-- Test of method sqDist ...");
 
+        // Test case 1: 1D
+        assert KDTree.sqDist(new double[]{1}, new double[]{4}) == 9.0 :
+            "Distance between [1] and [4] should be 9";
+
+        // Test case 2: 2D
+        assert KDTree.sqDist(new double[]{-1, 1}, new double[]{1, -1}) == 8.0 :
+            "Distance between [-1,1] and [1,-1] should be 8";
+
+        // Test case 3: 3D
+        assert KDTree.sqDist(new double[]{0, 0, 0}, new double[]{1, 2, 2}) == 9.0 :
+            "Distance between [0,0,0] and [1,2,2] should be 9";
+
+        // Test case 4: Same points
+        assert KDTree.sqDist(new double[]{1, 2, 3}, new double[]{1, 2, 3}) == 0.0 :
+            "Distance between same points should be 0";
+
+        // Test case 5: High dimension
+        double[] a = new double[1000];
+        double[] b = new double[1000];
+        for (int i = 0; i < 1000; i++) {
+            a[i] = 1;
+            b[i] = 0;
+        }
+        assert KDTree.sqDist(a, b) == 1000.0 :
+            "Distance between [1,...,1] and [0,...,0] in 1000D should be 1000";
+
+        System.out.println("[OK]");
+    }
 }
